f069f5fa8c878e990bbf9a2a4942c63e95da3152
Jannassl <Jan.nassling@metropolia.fi>
2024-01-16 14:19:49 +0200

    Fahrenheit muunnin

---
+++ src/Moduuli1/FarenheitToCelsius.java
@@ -0,0 +1,15 @@
+package Moduuli1;
+import java.util.Scanner;
+
+public class FarenheitToCelsius {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Syötä fahrenheit asteet: ");
+        double temp = Double.parseDouble(reader.nextLine());
+        double cels = (temp - 32) * 5/9;
+
+        System.out.printf("Celsius asteina: %.2f\n", cels);
+    }
+
+}
