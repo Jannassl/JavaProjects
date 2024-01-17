a3c9496d4200ca908684c1cff41e54961e5d663c
Jannassl <Jan.nassling@metropolia.fi>
2024-01-17 10:11:47 +0200

    Measurment units tehtävä tehty

---
+++ src/Moduuli1/MeasurmentUnits.java
@@ -0,0 +1,36 @@
+package Moduuli1;
+
+import java.text.DecimalFormat;
+
+public class MeasurmentUnits {
+    public static void main(String [] args) {
+        int grams = 30000;
+        double luoti = 13.28;
+        double naula = luoti * 32;
+        double leiviska = 20 * naula;
+
+        double jakoLeiviska = grams / leiviska;
+        int intJakoLeiviska = (int)jakoLeiviska;
+        double remaining = grams - (intJakoLeiviska * leiviska); //Remaining toimii.
+
+        double jakoNaula = remaining / naula;
+        int intJakoNaula = (int)jakoNaula;
+        double remaining2 = remaining -(intJakoNaula * naula);
+
+        double jakoLuoti = remaining2 / luoti;
+        DecimalFormat luodit = new DecimalFormat("###,##");
+
+        System.out.println(intJakoLeiviska+" Leiviskää");
+        System.out.println(intJakoNaula + " nauloja");
+        System.out.printf("%.2f\n",jakoLuoti);
+
+        System.out.printf("%d grams is %d leiviskä, %d naula and %.2f luoti",grams,intJakoLeiviska,intJakoNaula,jakoLuoti);
+
+
+
+
+
+
+
+    }
+}
