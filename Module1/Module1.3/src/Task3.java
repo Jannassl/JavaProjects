f710de9483b421a66cc5121b7ac3c6469ce0fe9b
Jannassl <Jan.nassling@metropolia.fi>
2024-01-18 09:59:15 +0200

    Module 1.3 task3 tehty

--- Module1/Module1.3/src/Task3.java
+++ Module1/Module1.3/src/Task3.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class Task3 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Anna ensimmäinen numero: ");
+        int first = Integer.parseInt(scanner.nextLine());
+        System.out.println("Anna toinen numero: ");
+        int second = Integer.parseInt(scanner.nextLine());
+        int i, j, flag;
+
+        for( i = first; i <= second; i++) {
+            if(i == 0 || i == 0)
+                continue;
+            flag = 1;
+
+            for(j = 2; j <= i / 2; j++) {
+                if(i % j == 0) {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag == 1)
+                System.out.println(i);
+        }
+
+    }
 }
