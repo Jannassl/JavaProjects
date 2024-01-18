eda62f6599e76ed10c0d29117f34def1de008a20
Jannassl <Jan.nassling@metropolia.fi>
2024-01-18 14:08:52 +0200

    Module 1.4 task2 tehty

--- Module1/Module1.4/src/Task2.java
+++ Module1/Module1.4/src/Task2.java
@@ -1,2 +1,42 @@
+import java.util.Scanner;
+
 public class Task2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of integers in the array: ");
+        int number = scanner.nextInt();
+
+        int[] array = new int[number];
+        System.out.println("Enter the integers into the array:");
+
+        for (int i = 0; i < number; i++) {
+            System.out.print("Element " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+        }
+
+        int maxSumma = Integer.MIN_VALUE;
+        int alkuLuku = 0, loppuLuku = 0;
+
+        for (int i = 0; i < number; i++) {
+            int nykySumma = 0;
+
+            for (int j = i; j < number; j++) {
+                nykySumma += array[j];
+
+                if (nykySumma > maxSumma) {
+                    maxSumma = nykySumma;
+                    alkuLuku = i;
+                    loppuLuku = j;
+                }
+            }
+        }
+
+        System.out.println("Maximum sum: " + maxSumma);
+        System.out.println("Subarray indices: " + (alkuLuku + 1) + " - " + (loppuLuku + 1));
+
+        scanner.close();
+    }
 }
+
