068126beac9967da60b91a94cd5221fa6d640319
Jan koti PC <Jan.nassling@metropolia.fi>
2024-01-22 12:27:28 +0200

    Moduuli 1.4 tehty

--- Module1/Module1.4/src/Task3.java
+++ Module1/Module1.4/src/Task3.java
@@ -1,21 +1,38 @@
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
+
 public class Task3 {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Anna arrayn pituus: ");
-        int size = Integer.parseInt(scanner.nextLine());
-        int arr [] = new int[size];
 
-        System.out.println("Syötä numerot: ");
-        for(int i =0; i<size; i++) {
-            arr[i] = scanner.nextInt();
+        System.out.print("Syötä arrayn pituus:  ");
+        int length = scanner.nextInt();
+
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++) {
+            System.out.print("Syötä numero: ");
+            array[i] = scanner.nextInt();
         }
 
-        int number = arr.length;
+        int[] uniqueArray = removeDuplicates(array);
 
-        if (number>1); {
+        System.out.println("Array ilman toistuvia numeroita " + Arrays.toString(uniqueArray));
 
+        scanner.close();
+    }
+    static int[] removeDuplicates(int[] array) {
+        Set<Integer> uniqueSet = new HashSet<>();
+        for (int num : array) {
+            uniqueSet.add(num);
         }
+        int[] uniqueArray = new int[uniqueSet.size()];
+        int index = 0;
+        for (int num : uniqueSet) {
+            uniqueArray[index++] = num;
+        }
+
+        return uniqueArray;
     }
 }
