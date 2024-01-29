00c3511b07534568abc5966fcd4b74f126561953
Jan koti PC <Jan.nassling@metropolia.fi>
2024-01-29 12:42:08 +0200

    Tehtävä 2.3.4

---
+++ Module2/Module2.3/Task2.3.4/src/Book.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Book {
+    private String title;
+    private String author;
+    private int publicationYear;
+    private double rating;
+
+    private ArrayList<String> reviews;
+
+
+    public Book(String title, String author, int publicationYear){
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.rating = 0.0;
+        this.reviews = new ArrayList<>();
+
+
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+    void addReview(String review){
+        reviews.add(review);
+    }
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+}
