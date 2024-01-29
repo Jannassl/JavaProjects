81e50703ad28fcbd8608deae8b07ad89aba05889
Jan koti PC <Jan.nassling@metropolia.fi>
2024-01-29 12:13:41 +0200

    Tehtävä 2.3.2

---
+++ Module2/Module2.3/Task3.2/src/LibraryMain.java
@@ -0,0 +1,18 @@
+public class LibraryMain {
+    public static void main(String[] args) {
+        Book book1 = new Book("Aku-Ankka","Matti Meikäläinen", 2010);
+        Book book2 = new Book("Taru sormusten herrasta", "J.R.R. Tolkien", 2001);
+        Book book3 = new Book("Fifty shades of grey", "E.L. James",2011);
+        Library library = new Library();
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+
+        library.displayBooks();
+
+        library.findBooksByAuthor("Matti Meikäläinen");
+        library.borrowBook("Taru sormusten herrasta");
+
+
+    }
+}
