4ea809f8b927d3084dfd938d1d5925a3bf160d49
Jannassl <Jan.nassling@metropolia.fi>
2024-01-30 11:46:37 +0200

    Module 3.1.4 tehty

---
+++ Module3/Module3.1/Task3.1.4/src/Rectangle.java
@@ -0,0 +1,12 @@
+class Rectangle extends Shape {
+    private double length;
+    private double width;
+
+
+    public Rectangle(double length, double width, String color) {
+        super(color);
+        this.length = length;
+        this.width = width;
+
+    }
+}
---
+++ Module3/Module3.1/Task3.1.4/src/Triangle.java
@@ -0,0 +1,15 @@
+class Triangle extends Shape {
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height, String color) {
+        super(color);
+        this.base = base;
+        this.height = height;
+
+    }
+    @Override
+    public double calculateArea() {
+        return 0.5 * base * height;
+    }
+}
