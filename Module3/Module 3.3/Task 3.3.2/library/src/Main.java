b54945fe743c90b313b190cf18c76b239f178c2f
Jannassl <Jan.nassling@metropolia.fi>
2024-02-07 12:42:31 +0200

    Task 3.3.2 tehty

---
+++ Module3/Module 3.3/Task 3.3.2/library/src/Main.java
@@ -0,0 +1,21 @@
+import model.Book;
+import model.LibraryMember;
+import system.Library;
+
+public class Main {
+    public static void main(String[] args) {
+        Library library = new Library();
+        LibraryMember libraryMember1 = new LibraryMember("Matti",1);
+        Book book1 = new Book("Aku ankka", "Pekka Haavisto", "123");
+        Book book2 = new Book("Ronja Ryövärin tytär", "Stubb", "321");
+
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addMembers(libraryMember1);
+
+        library.borrowBook(libraryMember1,book1);
+
+        library.returnBook(libraryMember1,book1);
+
+    }
+}
---
+++ Module3/Module 3.3/Task 3.3.2/library/src/system/Library.java
@@ -0,0 +1,26 @@
+package system;
+import model.Book;
+import model.LibraryMember;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<Book> books;
+    private List<LibraryMember> members;
+    public Library(){
+        this.books = new ArrayList<>();
+        this.members = new ArrayList<>();
+    }
+    public void addBook(Book book){
+        books.add(book);
+    }
+    public void addMembers(LibraryMember member){
+        members.add(member);
+    }
+    public void borrowBook(LibraryMember member, Book book){
+        books.add(book);
+    }
+    public void returnBook(LibraryMember member, Book book) {
+        books.remove(book);
+    }
+}
