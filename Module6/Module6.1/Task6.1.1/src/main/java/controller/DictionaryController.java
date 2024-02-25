9171ae404511c7043c4163d8e73c00f49c255365
Jan koti PC <Jan.nassling@metropolia.fi>
2024-02-25 21:15:18 +0200

    Update

--- Module6/Module6.1/Task6.1.1/src/main/java/controller/DictionaryController.java
+++ Module6/Module6.1/Task6.1.1/src/main/java/controller/DictionaryController.java
@@ -3,15 +3,18 @@ import model.Dictionary;
 import view.DictionaryView;
 
 
-public class DictionaryController {
+public class DictionaryController extends Dictionary {
     private Dictionary dictionary;
     private DictionaryView dictionaryView;
+
 
     public static void main(String[] args) {
         Dictionary dictionary1 = new Dictionary();
         dictionary1.addToDictionary("Apple", "A fruit");
         dictionary1.addToDictionary("Banana", "A yellow fruit");
         dictionary1.addToDictionary("Cat", "A pet animal");
+
+
 
         DictionaryView.launch(DictionaryView.class);
     }
--- Module6/Module6.1/Task6.1.1/src/main/java/view/DictionaryView.java
+++ Module6/Module6.1/Task6.1.1/src/main/java/view/DictionaryView.java
@@ -5,22 +5,27 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
+import model.Dictionary;
 
 public class DictionaryView extends Application{
+    private Dictionary dictionary;
     // Make a method to read user input and search for that word in dictionary
     public void start(Stage stage) {
         FlowPane rootNode = new FlowPane();
         Scene scene = new Scene(rootNode, 300, 200);
         stage.setScene(scene);
-        Label label = new Label("Enter word to search: ");
+        TextField word = new TextField("Enter word to search: ");
         Button button = new Button("Search");
-        rootNode.getChildren().addAll(label, button);
+        TextArea result = new TextArea();
+        rootNode.getChildren().addAll(word, button,result);
         stage.show();
         button.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
-                System.out.println("Search button clicked");
+                System.out.println();
             }
         });
     }
