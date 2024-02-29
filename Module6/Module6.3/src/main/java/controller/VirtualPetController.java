f03b8a215852e4344767e9c6dad48da6d460a029
Jannassl <Jan.nassling@metropolia.fi>
2024-02-29 18:38:44 +0200

    Update

--- Module6/Module6.3/src/main/java/controller/VirtualPetController.java
+++ Module6/Module6.3/src/main/java/controller/VirtualPetController.java
@@ -1,4 +1,31 @@
 package controller;
+import view.VirtualPetView;
+import model.VirtualPet;
+
 
 public class VirtualPetController {
+    private VirtualPet pet;
+    private VirtualPetView gui;
+
+    public VirtualPetController(VirtualPetView gui){
+        this.pet = new VirtualPet(0,0);
+        this.gui = gui;
+    }
+    public int getPlayerX() {
+        return pet.getX();
+    }
+
+    public int getPlayerY() {
+        return pet.getY();
+    }
+
+    public int getGridSize() {
+        return pet.getGridSize();
+    }
+
+    public static void main(String[] args) {
+        VirtualPetView.launch(VirtualPetView.class);
+    }
 }
+
+
--- Module6/Module6.3/src/main/java/view/VirtualPetView.java
+++ Module6/Module6.3/src/main/java/view/VirtualPetView.java
@@ -1,4 +1,47 @@
 package view;
 
-public class VirtualPetView {
+import controller.VirtualPetController;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
+public class VirtualPetView extends Application {
+    private static final int CELL_SIZE = 20;
+
+    private Canvas canvas;
+    private GraphicsContext gc;
+    private VirtualPetController controller;
+
+    @Override
+    public void start(Stage primaryStage) {
+        controller = new VirtualPetController(this);
+        int gridSize = controller.getGridSize();
+        int canvasSize = gridSize * CELL_SIZE;
+
+        canvas = new Canvas(canvasSize, canvasSize);
+        gc = canvas.getGraphicsContext2D();
+        StackPane root = new StackPane(canvas);
+        Scene scene = new Scene(root, canvasSize, canvasSize);
+        primaryStage.setTitle("Graphics Demo");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        updateCanvas(controller.getPlayerX(), controller.getPlayerY());
+    }
+    public void updateCanvas(int playerX, int playerY) {
+        int gridSize = controller.getGridSize();
+        int canvasSize = gridSize * CELL_SIZE;
+
+        gc.clearRect(0, 0, canvasSize, canvasSize);
+        Image image = new Image("/sadHamster.jpeg");
+        gc.drawImage(image, playerX * CELL_SIZE, playerY * CELL_SIZE);
+        gc.setFill(Color.BLUE);
+
+    }
 }
